8a23502cae6dd1bb229e975486f295538db059fb
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-23 10:42:03 -0500

    chore: enhance app configuration with HTTP client, animations, and PrimeNG setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,43 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig, inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {
+  AppInitializerService,
+  authenticationInterceptor
+} from '../shared';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+
+export const appInitializerProviders = () => {
+  const appInitializerService = inject(AppInitializerService);
+  return appInitializerService.initializeApp();
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authenticationInterceptor])
+    ),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false // Disable dark mode selector
+        }
+      }
+    }),
+    provideAppInitializer(appInitializerProviders),
   ]
 };
