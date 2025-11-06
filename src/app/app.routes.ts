09d45cc0735c60e964dcb04985c3ef27d72bdf07
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-06 14:24:06 -0500

    feat: add not found page route to handle undefined paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from '../pages/authentication/ui/login/login.page';
+import { authGuard, roleGuard } from '../shared';
+import { DashboardPage } from '../pages/dashboard/ui/dashboard/dashboard.page';
+import { AdminLayoutComponent } from '../features/layout/ui/admin-layout/admin-layout.component';
+import {NotFoundPage} from '../pages/authentication/ui/not-found/not-found.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // ==================== PUBLIC PAGES ====================
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+
+  // ==================== MAIN PAGES =========================
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    canActivate: [authGuard, roleGuard(['ROLE_SYSTEM_ADMINISTRATOR'])],
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+
+      // ==================== MAIN DASHBOARD ====================
+      {
+        path: 'dashboard',
+        component: DashboardPage,
+      },
+    ],
+  },
+
+  // ==================== NOT FOUND PAGE ====================
+  { path: '**',
+    component: NotFoundPage
+  }
+];
