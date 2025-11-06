d14101eac5e2749df14e60544a5b69c326ebe800
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-06 13:54:11 -0500

    feat: implement user authentication and account status management

---
+++ src/entities/user/model/entities/user.entity.ts
@@ -0,0 +1,15 @@
+import { AccountStatusEnum } from '../enums/account-status.enum';
+import { RoleEntity } from '../../../role/model';
+
+export interface UserEntity {
+  id: string;
+  username: string;
+  email: string;
+  password: string;
+  accountStatus: AccountStatusEnum;
+  failedLoginAttempts: number;
+  lastLoginAt: Date | null;
+  passwordChangedAt: Date | null;
+  roles: RoleEntity[];
+  token: string | null;
+}
