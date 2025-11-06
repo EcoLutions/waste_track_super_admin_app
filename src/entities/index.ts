15c30451b4e8256e878b5ec80dae71caaafd1502
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-06 13:54:55 -0500

    feat: add main barrel export for entities layer

---
+++ src/entities/index.ts
@@ -0,0 +1,12 @@
+// Entities Layer - Main barrel export
+
+// User Entity exports
+export * from './user/model';
+export * from './user/api';
+
+// Role Entity exports
+export * from './role/model';
+export * from './role/api';
+
+// Shared exports
+export * from '../shared/api';
