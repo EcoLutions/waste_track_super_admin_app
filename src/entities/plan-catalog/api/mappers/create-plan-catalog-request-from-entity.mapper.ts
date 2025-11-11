e5e142e998fac3b388901572f6293d024523ccc6
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-11 17:15:09 -0500

    feat(mapper): refactor plan catalog mappers to use EnumMapper
    - Replace CurrencyMapper and BillingPeriodMapper with EnumMapper
    - Simplify mapping logic for priceCurrency and billingPeriod
    - Ensure consistent enum handling across mappers

--- src/entities/plan-catalog/api/mappers/create-plan-catalog-request-from-entity.mapper.ts
+++ src/entities/plan-catalog/api/mappers/create-plan-catalog-request-from-entity.mapper.ts
@@ -1,15 +1,14 @@
 import {PlanCatalogEntity} from '@entities/plan-catalog/model';
 import {CreatePlanCatalogRequest} from '@entities/plan-catalog/api';
-import {CurrencyMapper} from '@shared/api/mappers/currency.mapper';
-import {BillingPeriodMapper} from '@shared/api/mappers/billing-period.mapper';
+import {EnumMapper} from '@shared/api/mappers/enum.mapper';
 
 export class CreatePlanCatalogRequestFromEntityMapper {
   static fromEntityToDto(entity: PlanCatalogEntity): CreatePlanCatalogRequest {
     return {
       name: entity.name,
       priceAmount: entity.priceAmount,
-      priceCurrency: CurrencyMapper.mapEnumToString(entity.priceCurrency),
-      billingPeriod: BillingPeriodMapper.mapEnumToString(entity.billingPeriod),
+      priceCurrency: EnumMapper.mapEnumToString(entity.priceCurrency),
+      billingPeriod: EnumMapper.mapEnumToString(entity.billingPeriod),
       maxVehicles: entity.maxVehicles,
       maxDrivers: entity.maxDrivers,
       maxContainers: entity.maxContainers,
--- src/entities/plan-catalog/api/mappers/plan-catalog-entity-from-response.mapper.ts
+++ src/entities/plan-catalog/api/mappers/plan-catalog-entity-from-response.mapper.ts
@@ -1,16 +1,17 @@
 import {PlanCatalogResponse} from '@entities/plan-catalog/api';
 import {PlanCatalogEntity} from '@entities/plan-catalog/model';
-import {BillingPeriodMapper} from '@shared/api/mappers/billing-period.mapper';
-import {CurrencyMapper} from '@shared/api/mappers/currency.mapper';
+import {BillingPeriodEnum, CurrencyEnum} from '@shared/model';
+import {EnumMapper} from '@shared/api/mappers/enum.mapper';
 
 export class PlanCatalogEntityFromResponseMapper {
   static fromDtoToEntity(dto: PlanCatalogResponse): PlanCatalogEntity {
+    console.log(dto);
     return {
       id: dto.id ?? '',
       name: dto.name ?? '',
       priceAmount: dto.priceAmount ?? 0,
-      priceCurrency: CurrencyMapper.mapStringToEnum(dto.priceCurrency ?? ''),
-      billingPeriod: BillingPeriodMapper.mapStringToEnum(dto.billingPeriod ?? ''),
+      priceCurrency: EnumMapper.mapStringToEnum(dto.priceCurrency, CurrencyEnum, CurrencyEnum.PEN),
+      billingPeriod: EnumMapper.mapStringToEnum(dto.billingPeriod, BillingPeriodEnum, BillingPeriodEnum.MONTHLY),
       maxVehicles: dto.maxVehicles ?? 0,
       maxDrivers: dto.maxDrivers ?? 0,
       maxContainers: dto.maxContainers ?? 0,
