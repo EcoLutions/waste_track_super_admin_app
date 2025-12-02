1cafff826e801795ce85e8b8a675c4cc96f75eb6
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-12-02 10:43:13 -0500

    chore(environment): update API URL for development and production environments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1/',
+  apiUrl: 'https://waste-track-platform-production.up.railway.app/api/v1/',
   enableDebugTools: true,
   logLevel: 'debug',
 };
